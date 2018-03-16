24f8118e8991a840833bd042181661a7f7d20ccd
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-03-16 09:39:37 -0400

    sign up specs

--- spec/app_controller/app_controller_spec.rb
+++ spec/app_controller/app_controller_spec.rb
@@ -13,6 +13,7 @@ describe ApplicationController do
       get '/signup'
       expect(last_response.status).to eq(200)
     end
+
     it 'directs user to index' do
       params = {
         :username => "lorem",
@@ -21,4 +22,49 @@ describe ApplicationController do
       }
       post '/signup', params
       expect(last_response.location).to include("/index")
+    end
+
+    it 'does not let a user sign up without a username' do
+      params = {
+        :username => "",
+        :email => "loremipsum@theinternet.com",
+        :password => "ipsum"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without an email' do
+      params = {
+        :username => "lorem",
+        :email => "",
+        :password => "ipsum"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a password' do
+      params = {
+        :username => "lorem",
+        :email => "loremipsum@theinternet.com",
+        :password => ""
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a logged in user view the signup page' do
+      user = User.create(:username => "lorem", :email => "loremipsum@theinternet.com", :password => "ipsum")
+      params = {
+        :username => "lorem",
+        :email => "loremipsum@theinternet.com"
+        :password => "ipsum"
+      }
+      post '/signup', params
+      session = {}
+      session[:user_id] = user.id
+      get '/signup'
+      expect(last_response.location).to include('/index')
+    end
 end

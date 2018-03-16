03575de62ee2bd1c19a9a870a96369011edb5ca4
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-03-16 09:36:19 -0400

    sign up specs

--- spec/app_controller/app_controller_spec.rb
+++ spec/app_controller/app_controller_spec.rb
@@ -7,4 +7,18 @@ describe ApplicationController do
       expect(last_response.status).to eq(200)
       #expect(last_response.body).to include ("Welcome to Book Library")
   end
+
+  describe "Sign Up" do
+    it 'loads the sign up page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+    it 'directs user to index' do
+      params = {
+        :username => "lorem",
+        :email => "loremipsum@theinternet.com"
+        :password => "ipsum"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/index")
 end

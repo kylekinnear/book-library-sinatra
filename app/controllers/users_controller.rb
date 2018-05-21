e029408ec1ee804f1da912d03cc0be82be31b799
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 11:27:12 -0400

    user post signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:password] != params[:password_check] || User.all.find{|email| email == params[:email]}
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/my-library'
+    end
+  end
+
 end

aa76cb901b1050d97c8b4b32662bb50574efc76a
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 13:28:02 -0400

    books post my library, still need tags and scraping

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,3 +9,37 @@ class BooksController < ApplicationController
       redirect to '/'
     end
   end
+
+  #we still need to add tags to the following:
+  post '/my-library' do
+    if logged_in?
+      if params[:title] != "" && params[:author] != ""
+        @book = current_user.books.new(title: params[:title], author: params[:author]) #is this correct?
+        if params[:read_it] == 1
+          @book.read? = 1
+          @book.times_read = params[:times_read]
+        end
+        if params[:own_it] == 1
+          @book.owned? = 1
+        end
+        if @book.location != ""
+          @book.location = params[:location]
+        end
+        if params[:comments] != ""
+          @book.comments = params[:comments]
+        end
+        if params[:get_goodreads?] == 1
+          #scrape
+        end
+        @book.save
+      else
+        redirect to '/my-library/new'
+      end
+      redirect to '/'
+    end
+  end
+
+
+
+
+end

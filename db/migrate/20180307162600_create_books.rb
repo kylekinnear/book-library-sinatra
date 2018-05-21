657376d8bb004e61bc0c99b7b0389f74e4569fba
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 10:43:01 -0400

    create books migration

--- db/migrate/20180307162600_create_books.rb
+++ db/migrate/20180307162600_create_books.rb
@@ -1,4 +1,19 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :tags
+      t.boolean :read?
+      t.integer :times_read
+      t.boolean :owned?
+      t.string :location
+      t.string :series
+      t.string :comments
+      t.boolean :goodreads_flag
+      t.string :goodreads_url
+      t.integer :goodreads_rates
+      t.float :goodreads_rating
+    end
   end
 end

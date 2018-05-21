40aa7c91e31676358193d990f5d2830b45e1a966
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 16:39:29 -0400

    author show route

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,18 @@
+require '.config/environment'
+
+class AuthorsController < ApplicationController
+
+  get '/authors/:id' do
+    if logged_in?
+      @author = Author.find_by_id(params[:id])
+      if @author
+        erb :'author/show_author'
+      else
+        redirect to '/my-library'
+      end
+    else
+      redirect to '/'
+    end
+  end
+
+end

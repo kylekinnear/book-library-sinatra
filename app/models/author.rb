1260221fe984f0b49e288efbca9c8c330cc28b56
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 16:51:57 -0400

    fixed author slug method

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ActiveRecord::Base
   has_many :books
 
   def slug
-    name.downcase.gsub(" ","").gsub(".","")
+    name.downcase.gsub(".","").gsub(" ","-")
   end
 
   def self.find_by_slug(slug)

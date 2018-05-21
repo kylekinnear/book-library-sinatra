8f328c2420b14e21774881d4719e418f7dc4a073
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-21 11:09:55 -0400

    added slug methods to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,11 @@
 class Author < ActiveRecord::Base
   has_many :books
+
+  def slug
+    name.downcase.gsub(" ","").gsub(".","")
+  end
+
+  def self.find_by_slug(slug)
+    Author.all.find{|author| author.slug == slug}
+  end
 end

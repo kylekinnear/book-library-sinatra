5002e301e220825751b30e8cf759d9cbdc6dcb91
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-22 13:07:19 -0400

    changed owned? and read? to owned_flag and read_flag in create books migration

--- db/migrate/003_create_books.rb
+++ db/migrate/003_create_books.rb
@@ -4,9 +4,9 @@ class CreateBooks < ActiveRecord::Migration[5.1]
       t.string :title
       t.integer :author_id
       t.string :tags
-      t.boolean :read?
+      t.boolean :read_flag
       t.integer :times_read
-      t.boolean :owned?
+      t.boolean :owned_flag
       t.string :location
       t.string :comments
       t.integer :user_id

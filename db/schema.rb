f5dc4bd5cf075ae30b63d20df065af85f305b90b
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-22 14:00:09 -0400

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
   end
 
   create_table "books", force: :cascade do |t|

99fb718030a6af9914043a2f32e0bd1b56aeb943
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-22 13:11:22 -0400

    migrations

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 4) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "author_id"
+    t.string "tags"
+    t.boolean "read_flag"
+    t.integer "times_read"
+    t.boolean "owned_flag"
+    t.string "location"
+    t.string "comments"
+    t.integer "user_id"
+    t.boolean "goodreads_flag"
+    t.string "goodreads_url"
+    t.string "goodreads_rating"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+end

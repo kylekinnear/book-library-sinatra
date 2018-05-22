d08475f7a0e72b4289a3c12e3ba4c5e0fa3975da
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-05-22 13:47:49 -0400

    trying new code to instantiate authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :books
 
-  def slug
+  def slugify
     name.downcase.gsub(".","").gsub(" ","-")
   end
 
@@ -9,8 +9,12 @@ class Author < ActiveRecord::Base
     Author.all.find{|author| author.slug == slug}
   end
 
-  def self.find_or_create_by_slug(slug, name)
-    Author.all.find_or_create_by{|author| author.slug == slug}
-    @self.name = name
+#  def self.find_or_create_by_slug(slug, name)
+    #Author.all.find_or_create_by{|author| author.slug == slug}
+#    @self.name = name
+#  end
+
+  def self.match_author(slug, name)
+    Author.where(self.slugify => slug).first_or_create(name: name)
   end
 end

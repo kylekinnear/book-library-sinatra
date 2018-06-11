d66f9117cff2ae9cb91e23301baeeb999c4566b3
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-06-11 11:05:07 -0400

    added flash messages to author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,11 @@ class AuthorsController < ApplicationController
       if @author
         erb :'authors/show_author'
       else
+        flash[:message] = "You don't have any books by that author."
         redirect to '/my-library'
       end
     else
+      flash[:message] = "Please log in."
       redirect to '/'
     end
   end

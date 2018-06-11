8da7ad2532e6e7005fc308ff73e0c6f239637149
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-06-10 20:17:11 -0400

    removed commented out edit button code from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -88,19 +88,6 @@ class BooksController < ApplicationController
     end
   end
 
-#  post '/my-library/:id/edit' do # commented out using links instead of buttons
-#    if logged_in?
-#      @book = Book.find_by_params(params[:id]) #is this going to work or is it going to try and look at the posted data?
-#      if @book && @book.user_id == current_user.id
-#        erb :'books/edit_book'
-#      else
-#        redirect to '/my-library'
-#      end
-#    else
-#      redirect to '/'
-#    end
-#  end
-
   delete '/my-library/:id/delete' do
     if logged_in?
       @book = Book.find_by_id(params[:id])

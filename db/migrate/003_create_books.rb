f899aca7b39b6cb3ed0b5d7be5c702b370308d2f
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-06-10 20:21:28 -0400

    removed goodreads and tags from migrations

--- db/migrate/003_create_books.rb
+++ db/migrate/003_create_books.rb
@@ -3,16 +3,12 @@ class CreateBooks < ActiveRecord::Migration[5.1]
     create_table :books do |t|
       t.string :title
       t.integer :author_id
-      t.string :tags
       t.boolean :read_flag
       t.integer :times_read
       t.boolean :owned_flag
       t.string :location
       t.string :comments
       t.integer :user_id
-      t.boolean :goodreads_flag
-      t.string :goodreads_url
-      t.string :goodreads_rating
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,12 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "author_id"
-    t.string "tags"
     t.boolean "read_flag"
     t.integer "times_read"
     t.boolean "owned_flag"
     t.string "location"
     t.string "comments"
     t.integer "user_id"
-    t.boolean "goodreads_flag"
-    t.string "goodreads_url"
-    t.string "goodreads_rating"
   end
 
   create_table "users", force: :cascade do |t|
